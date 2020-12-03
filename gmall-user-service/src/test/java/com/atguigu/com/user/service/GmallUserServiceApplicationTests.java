617c888988f2a6d31a99a8fa89a5832eb201755c
Administrator <mmg173083@163.com>
2020-12-03 21:23:28 +0800

    提交 demo

---
+++ gmall-user-service/src/test/java/com/atguigu/com/user/service/GmallUserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.com.user.service;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
